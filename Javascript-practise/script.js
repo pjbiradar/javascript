c64e85a34c677556549786720d9822500201a082
pooja biradar <pooja_biradar@softprodigy.com>
2025-10-22 13:57:15 +0530

    day 1&2 notes

---
+++ Javascript-practise/script.js
@@ -0,0 +1,48 @@
+
+// getName(); //it gives error that getName is not a function since we are using arrow functions and in memory
+//it saved as undefined since its a variable)
+// console.log(x);
+// var x= 8;
+//  function getName(){
+//     console.log("hey welcome to our website");
+//  }
+// var getName = ()=>{
+//    console.log("i'm arrow function");
+// }
+
+// getName();
+// console.log(x);
+
+
+
+//how function works in js
+
+// var y =10;
+// a();
+// b();
+// console.log(y);
+
+// function a(){
+//    var y= 17;
+//    console.log(y);
+// }
+
+// function b(){
+//    var y=67;
+//    console.log(y);
+// }
+
+
+
+
+///window and this (this === window)
+var x = 23;
+
+function y(){
+   var u =90;
+   
+}
+
+console.log(window.x);
+console.log(this.x);
+console.log(u);
