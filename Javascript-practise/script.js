8d2eb4b59939ffecee09925ba5da846b1d8b5f4f
pooja biradar <pooja_biradar@softprodigy.com>
2025-10-28 14:19:37 +0530

    october 28

--- Javascript-practise/script.js
+++ Javascript-practise/script.js
@@ -36,13 +36,73 @@
 
 
 ///window and this (this === window)
-var x = 23;
+// var x = 23;
 
-function y(){
-   var u =90;
+// function y(){
+//    var u =90;
    
-}
+// }
 
-console.log(window.x);
-console.log(this.x);
-console.log(u);
+// console.log(window.x);
+// console.log(this.x);
+// console.log(u);
+
+
+// undefined
+// const y = ()=>{
+//     console.log("hdhah");
+// }
+// let u = y();
+// console.log(u);
+
+
+
+
+//scopechain
+
+// function a(){
+//     var b = 78;
+//     function c(){
+//         var b = 8888;
+//         console.log(b);
+//     }
+   
+// }
+
+// var b = 1044;
+// a();
+
+
+
+
+
+
+
+
+
+//let and const
+// console.log(a);
+// console.log(b);
+// var a = 10;
+// let b = 888;
+
+
+
+//shadwoing
+
+// var a =89;
+// {
+//     var a = 33;
+//     let b = 899;
+//     const u= 90;
+//     console.log(a);
+//     console.log(b);
+//     console.log(u);
+// }
+// console.log(a); //here 33 variable is shadowed the global variale a = 89;
+//     console.log(b);
+//     console.log(u);
+
+    //but in case of let and const inside block the above scenario doesn't take place as let and const are block scoped its
+    //allocated in separte space  and it wont get  shadowed by outer variables
+
